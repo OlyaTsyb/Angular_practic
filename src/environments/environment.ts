8395ed0feaa8dfd5f0d994f1685578c77e114b88
Olya <olyabusol1605@gmail.com>
2021-02-12 18:44:03 +0200

    practic

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build --rod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API: 'http://192.168.88.31:8000'
 };
 
 /*
